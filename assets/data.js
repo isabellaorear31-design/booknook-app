87fe21f6f62c3287230dbfdda485e6db3e52ebe4
isabellaorear31-design <isabella.orear31@gmail.com>
2026-03-06 20:33:06 -0600

    Update data.js

--- assets/data.js
+++ assets/data.js
@@ -1,8 +1,32 @@
 export const romanceBooks = [
-    { id: 1, title: "Twilight", author: "Stephenie Meyer", series: "The Twilight Saga", year: 2005, trope: "Vampires" },
-    { id: 2, title: "New Moon", author: "Stephenie Meyer", series: "The Twilight Saga", year: 2006, trope: "Werewolves" },
-    { id: 3, title: "The Duke and I", author: "Julia Quinn", series: "Bridgerton", year: 2000, trope: "Fake Dating" },
-    { id: 4, title: "The Viscount Who Loved Me", author: "Julia Quinn", series: "Bridgerton", year: 2000, trope: "Enemies to Lovers" },
-    { id: 5, title: "Icebreaker", author: "Hannah Grace", series: "Maple Hills", year: 2022, trope: "Sports" },
-    { id: 6, title: "The Love Hypothesis", author: "Ali Hazelwood", series: "Standalone", year: 2021, trope: "STEM" }
+    { 
+        id: 1, 
+        title: "Twilight", 
+        author: "Stephenie Meyer", 
+        series: "The Twilight Saga", 
+        year: 2005, 
+        description: "Bella Swan moves to Forks and encounters Edward Cullen, a mysterious boy who reveals himself to be a vampire.",
+        stars: "⭐⭐⭐⭐",
+        image: "https://m.media-amazon.com/images/I/615ZI6mPqlL.jpg"
+    },
+    { 
+        id: 2, 
+        title: "The Duke and I", 
+        author: "Julia Quinn", 
+        series: "Bridgerton", 
+        year: 2000, 
+        description: "In the glittering halls of Regency London, Daphne Bridgerton and the Duke of Hastings form a fake courtship to secure their futures.",
+        stars: "⭐⭐⭐⭐⭐",
+        image: "https://m.media-amazon.com/images/I/81S6-39mE9L.jpg"
+    },
+    { 
+        id: 3, 
+        title: "Icebreaker", 
+        author: "Hannah Grace", 
+        series: "Maple Hills", 
+        year: 2022, 
+        description: "A competitive figure skater and a hockey captain are forced to share a rink, sparking an unexpected romance.",
+        stars: "⭐⭐⭐⭐",
+        image: "https://m.media-amazon.com/images/I/81p9860A6dL.jpg"
+    }
 ];
