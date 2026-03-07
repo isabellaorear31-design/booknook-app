8e4b2e34730d6c659d73ce43712147127629cf55
isabellaorear31-design <isabella.orear31@gmail.com>
2026-03-06 20:02:52 -0600

    Update data.js

--- assets/data.js
+++ assets/data.js
@@ -1,1 +1,42 @@
-
+const books = [
+  {
+    title: "The Love Hypothesis",
+    author: "Ali Hazelwood",
+    genre: "Romance"
+  },
+  {
+    title: "Beach Read",
+    author: "Emily Henry",
+    genre: "Romance"
+  },
+  {
+    title: "The Hating Game",
+    author: "Sally Thorne",
+    genre: "Romance"
+  },
+  {
+    title: "Red, White & Royal Blue",
+    author: "Casey McQuiston",
+    genre: "Romance"
+  },
+  {
+    title: "It Ends With Us",
+    author: "Colleen Hoover",
+    genre: "Romance"
+  },
+  {
+    title: "Love & Other Words",
+    author: "Christina Lauren",
+    genre: "Romance"
+  },
+  {
+    title: "Twisted Love",
+    author: "Ana Huang",
+    genre: "Romance"
+  },
+  {
+    title: "People We Meet on Vacation",
+    author: "Emily Henry",
+    genre: "Romance"
+  }
+];
