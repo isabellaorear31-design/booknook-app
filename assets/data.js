2cc4510af6c0b1647943fb0cc1585f8021e11a28
isabellaorear31-design <isabella.orear31@gmail.com>
2026-03-06 20:21:14 -0600

    Update data.js

--- assets/data.js
+++ assets/data.js
@@ -1,42 +1,8 @@
-const books = [
-  {
-    title: "The Love Hypothesis",
-    author: "Ali Hazelwood",
-    genre: "Romance"
-  },
-  {
-    title: "Beach Read",
-    author: "Emily Henry",
-    genre: "Romance"
-  },
-  {
-    title: "The Hating Game",
-    author: "Sally Thorne",
-    genre: "Romance"
-  },
-  {
-    title: "Red, White & Royal Blue",
-    author: "Casey McQuiston",
-    genre: "Romance"
-  },
-  {
-    title: "It Ends With Us",
-    author: "Colleen Hoover",
-    genre: "Romance"
-  },
-  {
-    title: "Love & Other Words",
-    author: "Christina Lauren",
-    genre: "Romance"
-  },
-  {
-    title: "Twisted Love",
-    author: "Ana Huang",
-    genre: "Romance"
-  },
-  {
-    title: "People We Meet on Vacation",
-    author: "Emily Henry",
-    genre: "Romance"
-  }
+export const romanceBooks = [
+    { id: 1, title: "Twilight", author: "Stephenie Meyer", series: "The Twilight Saga", year: 2005 },
+    { id: 2, title: "New Moon", author: "Stephenie Meyer", series: "The Twilight Saga", year: 2006 },
+    { id: 3, title: "The Duke and I", author: "Julia Quinn", series: "Bridgerton", year: 2000 },
+    { id: 4, title: "The Viscount Who Loved Me", author: "Julia Quinn", series: "Bridgerton", year: 2000 },
+    { id: 5, title: "Icebreaker", author: "Hannah Grace", series: "Maple Hills", year: 2022 },
+    { id: 6, title: "Book Lovers", author: "Emily Henry", series: "Standalone", year: 2022 }
 ];
