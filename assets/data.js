0e4513c94f4b1a55ccafed86a3c86482415831f5
isabellaorear31-design <isabella.orear31@gmail.com>
2026-03-06 20:27:47 -0600

    Update data.js

--- assets/data.js
+++ assets/data.js
@@ -1,8 +1,8 @@
 export const romanceBooks = [
-    { id: 1, title: "Twilight", author: "Stephenie Meyer", series: "The Twilight Saga", year: 2005 },
-    { id: 2, title: "New Moon", author: "Stephenie Meyer", series: "The Twilight Saga", year: 2006 },
-    { id: 3, title: "The Duke and I", author: "Julia Quinn", series: "Bridgerton", year: 2000 },
-    { id: 4, title: "The Viscount Who Loved Me", author: "Julia Quinn", series: "Bridgerton", year: 2000 },
-    { id: 5, title: "Icebreaker", author: "Hannah Grace", series: "Maple Hills", year: 2022 },
-    { id: 6, title: "Book Lovers", author: "Emily Henry", series: "Standalone", year: 2022 }
+    { id: 1, title: "Twilight", author: "Stephenie Meyer", series: "The Twilight Saga", year: 2005, trope: "Vampires" },
+    { id: 2, title: "New Moon", author: "Stephenie Meyer", series: "The Twilight Saga", year: 2006, trope: "Werewolves" },
+    { id: 3, title: "The Duke and I", author: "Julia Quinn", series: "Bridgerton", year: 2000, trope: "Fake Dating" },
+    { id: 4, title: "The Viscount Who Loved Me", author: "Julia Quinn", series: "Bridgerton", year: 2000, trope: "Enemies to Lovers" },
+    { id: 5, title: "Icebreaker", author: "Hannah Grace", series: "Maple Hills", year: 2022, trope: "Sports" },
+    { id: 6, title: "The Love Hypothesis", author: "Ali Hazelwood", series: "Standalone", year: 2021, trope: "STEM" }
 ];
