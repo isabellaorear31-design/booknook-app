5e1a8f1522524165f5eaa3aad5ecb06682215610
isabellaorear31-design <isabella.orear31@gmail.com>
2026-03-06 21:38:09 -0600

    Update data.js

--- assets/data.js
+++ assets/data.js
@@ -3,36 +3,36 @@ export const romanceBooks = [
         id: 1, title: "Twilight", author: "Stephenie Meyer", series: "The Twilight Saga", year: 2005, 
         stars: "⭐⭐⭐⭐⭐", 
         description: "“The classic tale of a girl and her sparkling protector.”<br><br>Bella Swan moves to Forks and encounters Edward Cullen, a mysterious boy who reveals himself to be a vampire.",
-        image: "https://m.media-amazon.com/images/I/615ZI6mPqlL.jpg"
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1700522826i/41865.jpg"
     },
     { 
         id: 2, title: "New Moon", author: "Stephenie Meyer", series: "The Twilight Saga", year: 2006, 
         stars: "⭐⭐⭐⭐", 
         description: "“When the sun sets, the shadows take over.”<br><br>Edward leaves Forks to protect Bella, leading her into a deep depression and a growing friendship with Jacob Black.",
-        image: "https://m.media-amazon.com/images/I/71p-GvH3YXL.jpg"
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1630592760i/49041.jpg"
     },
     { 
         id: 3, title: "The Duke and I", author: "Julia Quinn", series: "Bridgerton", year: 2000, 
         stars: "⭐⭐⭐⭐⭐", 
         description: "“A fake courtship that set the Ton on fire.”<br><br>Daphne Bridgerton and Simon Basset, the Duke of Hastings, form a plan to keep matchmakers away, only to find real sparks.",
-        image: "https://m.media-amazon.com/images/I/81S6-39mE9L.jpg"
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1379594717i/110391.jpg"
     },
     { 
         id: 4, title: "The Viscount Who Loved Me", author: "Julia Quinn", series: "Bridgerton", year: 2000, 
         stars: "⭐⭐⭐⭐⭐", 
         description: "“He met his match in the woman he tried to avoid.”<br><br>Anthony Bridgerton has chosen a wife, but her older sister Kate Sheffield is determined to stop the marriage.",
-        image: "https://m.media-amazon.com/images/I/81fHwAnF66L.jpg"
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1379594308i/861326.jpg"
     },
     { 
         id: 5, title: "Icebreaker", author: "Hannah Grace", series: "Maple Hills", year: 2022, 
         stars: "⭐⭐", 
         description: "“A hockey romance that proves the coldest hearts can melt on the ice.”<br><br>Anastasia is a competitive figure skater and Nate is a hockey captain. They are forced to share a rink and a lot more.",
-        image: "https://m.media-amazon.com/images/I/81p9860A6dL.jpg"
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1659045816i/61767292.jpg"
     },
     { 
         id: 6, title: "Book Lovers", author: "Emily Henry", series: "Standalone", year: 2022, 
         stars: "⭐⭐⭐", 
         description: "“Two rivals, one small town, and a story neither expected.”<br><br>Nora is a cutthroat literary agent who keeps bumping into Charlie, a grumpy editor, while on vacation in a small town.",
-        image: "https://m.media-amazon.com/images/I/71p0v7BvjUL.jpg"
+        image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1638867089i/58690308.jpg"
     }
 ];
